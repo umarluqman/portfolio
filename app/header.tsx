a2248390f6151f73b5ffbcd5b5e0cb10aa07d7c2
umarluqman <umarluqman.78@gmail.com>
2025-04-08 15:51:39 +0800

    update

--- app/header.tsx
+++ app/header.tsx
@@ -16,7 +16,7 @@ export function Header() {
           className="text-zinc-600 dark:text-zinc-500"
           delay={0.5}
         >
-          Bitcoin-only Frontend Engineer
+          Frontend Engineer
         </TextEffect>
       </div>
     </header>
